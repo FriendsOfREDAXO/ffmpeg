a3f3793ee24ee3af251f6b9db0d77f9357f370dd
Thomas Skerbis <thomas.skerbis@mac.com>
2020-07-08 13:53:31 +0200

    defaults geändert

--- install.php
+++ install.php
@@ -3,5 +3,5 @@
 /** @var rex_addon $this */
 
 if (!$this->hasConfig()) {
-    $this->setConfig('command', 'ffmpeg -i INPUT OUTPUT');
+    $this->setConfig('command', 'ffmpeg -i INPUT -vcodec h264   OUTPUT.mp4');
 }
