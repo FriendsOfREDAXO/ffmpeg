359bed8f1d1ab310c3994843064ff771c8f9a2cf
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-09 18:36:54 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 (function ($) {
     var doProgress;
+    var conversionActive = false;
 
     function SetProgressStart() {
         doProgress = setInterval(
@@ -7,14 +8,14 @@
     }
 
     function scrolllog() {
-        $('#log pre').scrollTop($('#log pre')[0].scrollHeight)
+        $('#log pre').scrollTop($('#log pre')[0].scrollHeight);
     }
 
     function showProgress() {
         $.ajax({
             type: 'get',
-            url: './index.php?ffmpeg_video=1&progress=1',
-            dataType: 'json', // data type
+            url: 'index.php?rex-api-call=ffmpeg_converter&func=progress',
+            dataType: 'json',
         })
             .fail(function (jqXHR, textStatus) {
                 console.log("Request failed: " + textStatus);
@@ -22,25 +23,31 @@
                 $('#log pre').html("Request failed: " + textStatus);
                 $('#start').removeClass('disabled');
                 clearInterval(doProgress);
-
+                conversionActive = false;
             })
             .done(function (data) {
-                $('#log pre').html(data.log);
+                if (data.error) {
+                    $('#log pre').html("Error: " + data.error);
+                    $('#start').removeClass('disabled');
+                    clearInterval(doProgress);
+                    conversionActive = false;
+                    return;
+                }
 
+                $('#log pre').html(data.log);
                 scrolllog();
 
                 if (data.log.indexOf("Overwrite ?") >= 0) {
-
                     $('#start').removeClass('disabled');
                     clearInterval(doProgress);
-
+                    conversionActive = false;
                 } else {
-
                     if (data.progress === "done") {
                         $('#prog').html('100%');
                         $('#prog').css('width', '100%');
                         $('#start').removeClass('disabled');
                         clearInterval(doProgress);
+                        conversionActive = false;
                         showFinalDone();
                     } else {
                         $('#prog').css('width', data.progress + '%');
@@ -53,24 +60,33 @@
     function showFinalDone() {
         $.ajax({
             type: 'get',
-            url: './index.php?ffmpeg_video=1&done=1',
-            dataType: 'json', // data type
+            url: 'index.php?rex-api-call=ffmpeg_converter&func=done',
+            dataType: 'json',
         })
             .fail(function (jqXHR, textStatus) {
                 console.log("Request failed: " + textStatus);
-
                 $('#log pre').html("Request failed: " + textStatus);
-
             })
             .done(function (data) {
+                if (data.error) {
+                    $('#log pre').html("Error: " + data.error);
+                    return;
+                }
                 $('#log pre').html(data.log);
+                scrolllog();
             });
     }
 
     $(document).ready(function () {
+        // Check if there's an active conversion on page load
+        if (conversionActive) {
+            $('.log').show();
+            $('.progress-section').show();
+            $('.progress').show();
+            SetProgressStart();
+        }
 
         $('#start').on('click', function () {
-
             let video = $('input[name=video]:checked').val();
 
             if (video === undefined) {
@@ -79,29 +95,53 @@
             }
 
             $(this).addClass('disabled');
+            conversionActive = true;
 
             $.ajax({
                 type: 'get',
-                url: './index.php?ffmpeg_video=1&start=1&video=' + video
+                url: 'index.php?rex-api-call=ffmpeg_converter&func=start&video=' + encodeURIComponent(video),
+                dataType: 'json'
             })
                 .fail(function (jqXHR, textStatus) {
                     console.log("Request failed: " + textStatus);
-
-                    $('.log').show()
+                    $('.log').show();
                     $('.progress-section').show();
-
                     $('#log pre').html("Request failed: " + textStatus);
-
+                    $('#start').removeClass('disabled');
+                    conversionActive = false;
                 })
-                .done(function (msg) {
+                .done(function (data) {
+                    if (data.error) {
+                        $('.log').show();
+                        $('.progress-section').show();
+                        $('#log pre').html("Error: " + data.error);
+                        $('#start').removeClass('disabled');
+                        conversionActive = false;
+                        return;
+                    }
+
                     $('.log').show();
                     $('.progress-section').show();
                     $('.progress').show();
+                    $('#prog').css('width', '0%');
+                    $('#prog').html('0%');
+                    $('#log pre').html("Conversion started...");
                     SetProgressStart();
-                })
+                });
 
             return false;
-        })
+        });
 
+        // Optional: Add a button to check status of ongoing conversions
+        $('#check_status').on('click', function() {
+            if (!conversionActive) {
+                conversionActive = true;
+                $('.log').show();
+                $('.progress-section').show();
+                $('.progress').show();
+                SetProgressStart();
+            }
+            return false;
+        });
     });
 })(jQuery);
