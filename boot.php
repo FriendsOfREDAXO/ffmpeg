169152f628ab58e5739c03f841013d9743b91afc
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-09 21:01:41 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -4,7 +4,7 @@ if (rex::isBackend() && rex::getUser()) {
     // Add JavaScript to ffmpeg page
     if (rex_be_controller::getCurrentPagePart(2) == 'ffmpeg') {
         rex_view::addJsFile(rex_addon::get('ffmpeg')->getAssetsUrl('js/script.js'));
-        rex_view::addCssFile(rex_addon::get('ffmpeg')->getAssetsUrl('style.css'));
+        rex_view::addCssFile(rex_addon::get('ffmpeg')->getAssetsUrl('css/style.css'));
     }
     
     // Create session variables if needed
