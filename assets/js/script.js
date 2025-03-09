e6ce976e7290841a168e7f9c2a383e7e851b68e0
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-09 18:52:51 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@
     var conversionActive = false;
 
     function SetProgressStart() {
-        doProgress = setInterval(
-            showProgress, 2000);
+        doProgress = setInterval(showProgress, 2000);
     }
 
     function scrolllog() {
@@ -19,18 +18,19 @@
         })
             .fail(function (jqXHR, textStatus) {
                 console.log("Request failed: " + textStatus);
-
                 $('#log pre').html("Request failed: " + textStatus);
-                $('#start').removeClass('disabled');
+                $('#start').removeClass('disabled').prop('disabled', false);
                 clearInterval(doProgress);
                 conversionActive = false;
+                updateUIForConversion(false);
             })
             .done(function (data) {
                 if (data.error) {
                     $('#log pre').html("Error: " + data.error);
-                    $('#start').removeClass('disabled');
+                    $('#start').removeClass('disabled').prop('disabled', false);
                     clearInterval(doProgress);
                     conversionActive = false;
+                    updateUIForConversion(false);
                     return;
                 }
 
@@ -38,23 +38,53 @@
                 scrolllog();
 
                 if (data.log.indexOf("Overwrite ?") >= 0) {
-                    $('#start').removeClass('disabled');
+                    $('#start').removeClass('disabled').prop('disabled', false);
                     clearInterval(doProgress);
                     conversionActive = false;
+                    updateUIForConversion(false);
                 } else {
                     if (data.progress === "done") {
-                        $('#prog').html('100%');
                         $('#prog').css('width', '100%');
-                        $('#start').removeClass('disabled');
+                        $('#progress-text').html('100%');
+                        $('#start').removeClass('disabled').prop('disabled', false);
+                        
+                        // Zeige Erfolgsanimation
+                        $('.spinner').hide();
+                        $('#progress-text').addClass('text-success').html('<i class="fa fa-check"></i> Fertig!');
+                        
                         clearInterval(doProgress);
                         conversionActive = false;
+                        updateUIForConversion(false);
                         showFinalDone();
+                        
+                        // Nach 3 Sekunden Seite neu laden, um die optimierte Video-Liste zu aktualisieren
+                        setTimeout(function() {
+                            window.location.reload();
+                        }, 3000);
                     } else {
-                        $('#prog').css('width', data.progress + '%');
-                        $('#prog').html(data.progress + '%');
+                        var progress = parseInt(data.progress);
+                        $('#prog').css('width', progress + '%');
+                        $('#progress-text').html(progress + '%');
                     }
                 }
             });
+    }
+
+    function updateUIForConversion(active) {
+        conversionActive = active;
+        
+        if (active) {
+            // UI für aktive Konvertierung
+            $('.progress-section').show();
+            $('input[name=video]').prop('disabled', true);
+            $('#start').addClass('disabled').prop('disabled', true);
+            $('.spinner').show();
+            $('#progress-text').removeClass('text-success').html('0%');
+        } else {
+            // UI für inaktive Konvertierung, aber Fortschrittsanzeige bleibt sichtbar
+            $('input[name=video]').prop('disabled', false);
+            $('#start').removeClass('disabled').prop('disabled', false);
+        }
     }
 
     function showFinalDone() {
@@ -78,24 +108,18 @@
     }
 
     $(document).ready(function () {
-        // Check if there's an active conversion on page load
-        if (conversionActive) {
-            $('.log').show();
-            $('.progress-section').show();
-            $('.progress').show();
-            SetProgressStart();
-        }
-
+        // Prüfe Server-Status bei Seitenladung
+        checkServerConversionStatus();
+        
         $('#start').on('click', function () {
             let video = $('input[name=video]:checked').val();
 
             if (video === undefined) {
-                alert('Please Select Video File for Start Operation!');
+                alert($('.rex-addon-output').data('i18n-select-video') || 'Bitte wählen Sie ein Video zur Konvertierung aus!');
                 return false;
             }
 
-            $(this).addClass('disabled');
-            conversionActive = true;
+            updateUIForConversion(true);
 
             $.ajax({
                 type: 'get',
@@ -104,44 +128,53 @@
             })
                 .fail(function (jqXHR, textStatus) {
                     console.log("Request failed: " + textStatus);
-                    $('.log').show();
                     $('.progress-section').show();
                     $('#log pre').html("Request failed: " + textStatus);
-                    $('#start').removeClass('disabled');
-                    conversionActive = false;
+                    updateUIForConversion(false);
                 })
                 .done(function (data) {
                     if (data.error) {
-                        $('.log').show();
                         $('.progress-section').show();
                         $('#log pre').html("Error: " + data.error);
-                        $('#start').removeClass('disabled');
-                        conversionActive = false;
+                        updateUIForConversion(false);
                         return;
                     }
 
-                    $('.log').show();
                     $('.progress-section').show();
-                    $('.progress').show();
                     $('#prog').css('width', '0%');
-                    $('#prog').html('0%');
-                    $('#log pre').html("Conversion started...");
+                    $('#progress-text').html('0%');
+                    $('#log pre').html("Konvertierung gestartet...");
                     SetProgressStart();
                 });
 
             return false;
         });
 
-        // Optional: Add a button to check status of ongoing conversions
+        // Status-Button
         $('#check_status').on('click', function() {
-            if (!conversionActive) {
-                conversionActive = true;
-                $('.log').show();
-                $('.progress-section').show();
-                $('.progress').show();
-                SetProgressStart();
-            }
+            checkServerConversionStatus();
             return false;
         });
+        
+        // Video-Auswahl etwas schöner machen
+        $('input[name=video]').change(function() {
+            $('.video-item').removeClass('active-video');
+            $(this).closest('.video-item').addClass('active-video');
+        });
     });
+    
+    function checkServerConversionStatus() {
+        $.ajax({
+            type: 'get',
+            url: 'index.php?rex-api-call=ffmpeg_converter&func=status',
+            dataType: 'json'
+        })
+            .done(function (data) {
+                if (data.active) {
+                    conversionActive = true;
+                    updateUIForConversion(true);
+                    SetProgressStart();
+                }
+            });
+    }
 })(jQuery);
