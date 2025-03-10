46a9c129350dba6fd9bd7f516cf12ec0db9bed3c
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-10 11:08:04 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -11,4 +11,52 @@ if (rex::isBackend() && rex::getUser()) {
     if (is_null(rex_session('ffmpeg_uid', 'string', null))) {
         rex_set_session('ffmpeg_uid', uniqid());
     }
+    
+    // Sicherstellen, dass die Datenverzeichnisse existieren
+    $dataPath = rex_addon::get('ffmpeg')->getDataPath();
+    if (!file_exists($dataPath)) {
+        mkdir($dataPath, 0777, true);
+    }
+    
+    // Veraltete Status- und Log-Dateien aufräumen (älter als 7 Tage)
+    $cleanupTime = time() - (7 * 24 * 60 * 60); // 7 Tage
+    
+    // Status-Dateien aufräumen
+    $statusFiles = glob($dataPath . 'status_*.json');
+    foreach ($statusFiles as $file) {
+        if (filemtime($file) < $cleanupTime) {
+            @unlink($file);
+        }
+    }
+    
+    // Log-Dateien aufräumen
+    $logFiles = glob($dataPath . 'log*.txt');
+    foreach ($logFiles as $file) {
+        if (filemtime($file) < $cleanupTime) {
+            @unlink($file);
+        }
+    }
+    
+    // Prüfen auf aktive Konvertierungen ohne Session
+    // Wenn ein Video gerade konvertiert wird, aber die Session abgelaufen ist,
+    // kann durch diese Prüfung trotzdem der Status abgefragt werden
+    if (rex_be_controller::getCurrentPagePart(2) == 'ffmpeg' && !rex_session('ffmpeg_conversion_id')) {
+        $statusFiles = glob($dataPath . 'status_*.json');
+        foreach ($statusFiles as $file) {
+            // Nur Dateien prüfen, die in den letzten 30 Minuten geändert wurden
+            if (filemtime($file) > time() - 1800) {
+                $statusData = json_decode(file_get_contents($file), true);
+                if ($statusData && isset($statusData['status']) && 
+                    $statusData['status'] !== 'done' && $statusData['status'] !== 'error') {
+                    
+                    // Aktive Konvertierung gefunden, Session-Variablen wiederherstellen
+                    if (isset($statusData['conversion_id'])) {
+                        rex_set_session('ffmpeg_conversion_id', $statusData['conversion_id']);
+                        rex_set_session('ffmpeg_conversion_status', $statusData['status']);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
